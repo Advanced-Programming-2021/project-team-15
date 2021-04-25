151c9bb065bec286f1fe672bcc1b79bf3e4fd626
Unknown <unknown@example.com>
2021-04-25 13:11:45 -0700

    nothing special

---
+++ src/main/java/model/NumbericZone.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.HashMap;
+
+public class NumbericZone extends Zone {
+    private HashMap zoneCards;
+    public class NumbericZ
+
+    public HashMap getZoneCards() {
+        return zoneCards;
+    }
+
+    public void setZoneCards(HashMap zoneCards) {
+        this.zoneCards = zoneCards;
+    }<Integer , Card >;
+}
