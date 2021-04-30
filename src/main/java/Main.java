9a89fc3770b68bb1544116c01829def893c8ac2b
Mehrad <milanloo@ce.sharif.edu>
2021-04-30 15:19:20 +0430

    finish card json

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,32 @@
 import controller.JSONController;
 import model.Card;
+import model.MagicCard;
 import model.MonsterCard;
 import view.LoginMenu;
 
 public class Main {
     public static void main(String[] args) {
         JSONController jsonController = new JSONController();
-      //  jsonController.MonsterCardParseJson();
+        jsonController.MonsterCardParseJson();
         jsonController.MagicCardParseJson();
         for (Card card : Card.getAllCards()) {
-            MonsterCard monsterCard = (MonsterCard)card;
-            System.out.println("Card Name : "+ monsterCard.getCardName());
-            System.out.println("Card Type : "+monsterCard.getCardType());
-            System.out.println("Level : "+ monsterCard.getLevel());
-            System.out.println("Monster Type : "+ monsterCard.getMonsterAttribute());
-            System.out.println("Card Type : "+ monsterCard.getMonsterEffectType());
-            System.out.println("Attribute : "+ monsterCard.getMonsterAttribute());
+
+            if (card.getCardType()== Card.CardType.MONSTER) {
+                MonsterCard monsterCard = (MonsterCard) card;
+                System.out.println("Card Name : " + monsterCard.getCardName());
+                System.out.println("Type : " + monsterCard.getCardType());
+                System.out.println("Level : " + monsterCard.getLevel());
+                System.out.println("Monster Type : " + monsterCard.getMonsterAttribute());
+                System.out.println("Card Type : " + monsterCard.getMonsterEffectType());
+                System.out.println("Attribute : " + monsterCard.getMonsterAttribute());
+            }
+            else {
+                MagicCard magicCard = (MagicCard) card;
+                System.out.println("Card Name : " + magicCard.getCardName());
+                System.out.println("Type : " + magicCard.getMagicType());
+                System.out.println("Icon : " + magicCard.getCardIcon());
+                System.out.println("Status : " + magicCard.getStatus());
+            }
             System.out.println("-----------------------------------------------");
         }
         LoginMenu loginMenu = new LoginMenu("Login Menu");
--- src/main/java/controller/JSONController.java
+++ src/main/java/controller/JSONController.java
@@ -2,11 +2,9 @@ package controller;
 
 import com.google.gson.*;
 import model.*;
-
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.lang.reflect.Type;
 
 public class JSONController {
     public void MonsterCardParseJson() {
@@ -17,40 +15,40 @@ public class JSONController {
                 monsterCard.setCardType(Card.CardType.MONSTER);
                 Card.addCard(monsterCard);
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    static class MagicCardDeserializer implements JsonDeserializer<MagicCard> {
-        @Override
-        public MagicCard deserialize(JsonElement json, Type typeof, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
-            JsonObject jsonObject = json.getAsJsonObject();
-
-            JsonElement jsonType = jsonObject.get("Type");
-            String type = jsonType.getAsString();
-
-            MagicCard magicCard = null;
-
-            if("Trap".equals(type)) {
-                System.out.println("trap");;
-            } else if("Spell".equals(type)) {
-                System.out.println("spell");
-            }
-            // TODO : set properties of type model
-
-            return magicCard;
-        }
-    }
+//    static class MagicCardDeserializer implements JsonDeserializer<MagicCard> {
+//        @Override
+//        public MagicCard deserialize(JsonElement json, Type typeof, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+//            JsonObject jsonObject = json.getAsJsonObject();
+//
+//            JsonElement jsonType = jsonObject.get("Type");
+//            String type = jsonType.getAsString();
+//
+//            MagicCard magicCard = null;
+//
+//            if("Trap".equals(type)) {
+//                magicCard = new TrapCard();
+//                magicCard.setMagicType(MagicCard.MagicType.TRAP);
+//            } else if("Spell".equals(type)) {
+//                magicCard = new SpellCard();
+//                magicCard.setMagicType(MagicCard.MagicType.SPELL);
+//            }
+//            return magicCard;
+//        }
+//    }
     public void MagicCardParseJson() {
         GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.registerTypeAdapter(MagicCard.class, new MagicCardDeserializer());
+//        gsonBuilder.registerTypeAdapter(MagicCard.class, new MagicCardDeserializer());
         Gson gson = gsonBuilder.create();
         try (Reader reader = new FileReader("Magic.json")) {
             MagicCard[] magicCardArray = gson.fromJson(reader, MagicCard[].class);
             for (MagicCard magicCard : magicCardArray) {
-                System.out.println(magicCard.getCardName());
+                magicCard.setCardType(Card.CardType.MAGIC);
+                Card.addCard(magicCard);
             }
         }
         catch (IOException e) {
