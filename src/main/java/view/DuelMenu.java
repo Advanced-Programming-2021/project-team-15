b59055d08e69b8729105010e2a90a51654a3580f
Unknown <unknown@example.com>
2021-05-02 14:34:34 -0700

    used singleton for duel menu

--- src/main/java/view/DuelMenu.java
+++ src/main/java/view/DuelMenu.java
@@ -1,2 +1,20 @@
-package view;public class duelMenu {
+package view;
+
+public class DuelMenu extends Menu{
+    private static DuelMenu duelMenu;
+    private DuelMenu()
+    {
+        super("Duel Menu");
+    }
+    public static DuelMenu getInstance()
+    {
+        if(duelMenu==null)
+            duelMenu  =new DuelMenu();
+        return duelMenu;
+    }
+
+    @Override
+    public void scanInput() {
+
+    }
 }
