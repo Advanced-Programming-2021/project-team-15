9a829a4fe501b50932cf491033d66314a42ed244
parnian <razaviparnian81@yahoo.com>
2021-07-02 05:30:46 -0700

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,57 +1,36 @@
 import com.opencsv.exceptions.CsvValidationException;
-import controller.JSONController;
-import model.Card;
-import model.MagicCard;
-import model.MonsterCard;
+import controller.utilizationController.DatabaseController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import view.LoginMenu;
 
 import java.io.IOException;
 
 /*To Handle :
-menu entry from other menus
 giving other menus instructions
-shop buy item clone cards
-Import/Export Menu???
 card show (page 10)
 */
 //TODO check:
 //attack controller line 102 - attackToDefencePos return
 
+public class Main extends Application {
+    public static Stage stage;
 
-
-
-
-public class Main {
     public static void main(String[] args) throws IOException, CsvValidationException {
-        JSONController jsonController = new JSONController();
-        jsonController.loadGameCards();
+        DatabaseController databaseController = new DatabaseController();
+        databaseController.loadGameCards();
         LoginMenu loginMenu = LoginMenu.getInstance();
-//        for (Card card : Card.getAllCards()) {
-//            if (card instanceof MonsterCard) System.out.println("monster");
-//            else if (card instanceof MagicCard) System.out.println("magic");
-//            else System.out.println("card");
-//        }
-        loginMenu.scanInput();
 
-//        for (Card card : Card.getAllCards()) {
-//
-//            if (card.getCardType()== Card.CardType.MONSTER) {
-//                MonsterCard monsterCard = (MonsterCard) card;
-//                System.out.println("Card Name : " + monsterCard.getCardName());
-//                System.out.println("Type : " + monsterCard.getCardType());
-//                System.out.println("Level : " + monsterCard.getLevel());
-//                System.out.println("Monster Type : " + monsterCard.getMonsterAttribute());
-//                System.out.println("Card Type : " + monsterCard.getMonsterEffectType());
-//                System.out.println("Attribute : " + monsterCard.getMonsterAttribute());
-//            }
-//            else {
-//                MagicCard magicCard = (MagicCard) card;
-//                System.out.println("Card Name : " + magicCard.getCardName());
-//                System.out.println("Type : " + magicCard.getMagicType());
-//                System.out.println("Icon : " + magicCard.getCardIcon());
-//                System.out.println("Status : " + magicCard.getStatus());
-//            }
-//            System.out.println("-----------------------------------------------");
-//        }
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        this.stage= stage;
+        Parent root = FXMLLoader.load(getClass().getResource("/FxmFiles/Welcome.fxml"));
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
