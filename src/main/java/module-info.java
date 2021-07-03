2f5e89d317adda9cb67dad7bd2f5dab173d3e059
parnian <razaviparnian81@yahoo.com>
2021-07-03 05:33:45 -0700

    ://

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,2 @@
-module YuGiOhG15 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires gson;
-    requires json;
-    requires java.sql;
-    requires javafx.media;
-    requires opencsv;
-    requires cloning;
-    requires gson.extras;
-    opens view to javafx.fxml;
-    opens model to javafx.base;
-    exports view;
-    exports controller;
-    opens controller to javafx.fxml;
+module $MODULE_NAME$ {
 }
