82d619d0d62be1dde2184e916e71acdffe543a64
parnian <razaviparnian81@yahoo.com>
2021-07-19 05:50:55 -0700

    Hmmm

---
+++ Client/src/main/java/sample/controller/ClientManager.java
@@ -0,0 +1,40 @@
+package sample.controller;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientManager {
+    private  static DataInputStream dataInputStream;
+    private  static DataOutputStream dataOutputStream;
+    public  static void run() {
+        try {
+            Socket socket = new Socket("localhost", 8000);
+            dataInputStream = new DataInputStream(socket.getInputStream());
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            dataOutputStream.flush();
+        } catch ( IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void send(String input) {
+        try {
+            dataOutputStream.writeUTF(input);
+            dataOutputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String sendAndGetResponse(String input) {
+        try {
+            send(input);
+            return dataInputStream.readUTF();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
